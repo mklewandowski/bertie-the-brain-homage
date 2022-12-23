5816017c1b00a72629290a34228d2c4dea9dfb91
mklewandowski <matt.kain.lewandowski@gmail.com>
2022-12-23 14:22:28 -0800

    Add getWinner helper function to vet the gameboard for completion

--- src/utils.ts
+++ src/utils.ts
@@ -14,3 +14,54 @@ export const getBertieMove = (gameState: string[], difficulty: number) => {
   }
   return bertieSquare;
 }
+
+export const getWinner = (gameState: string[]) => {
+  const gridSize = 3;
+
+  let computerWin = false;
+  let playerWin = false;
+
+  // check rows
+  for (let r = 0; r < gridSize; r++) {
+    let computerPoints = 0;
+    let playerPoints = 0;
+    for (let cell = 0; cell < gridSize; cell++)
+    {
+      if (gameState[r * gridSize + cell] === "X")
+        playerPoints++;
+      else if (gameState[r * gridSize + cell] === "O")
+        computerPoints++;
+    }
+    if (playerPoints >= 3)
+      playerWin = true;
+    else if (computerPoints >= 3)
+    computerWin = true;
+  }
+
+  // check cols
+  for (let c = 0; c < gridSize; c++) {
+    let computerPoints = 0;
+    let playerPoints = 0;
+    for (let cell = 0; cell < gridSize; cell++)
+    {
+      if (gameState[c + gridSize * cell] === "X")
+        playerPoints++;
+      else if (gameState[c + gridSize * cell] === "O")
+        computerPoints++;
+    }
+    if (playerPoints >= 3)
+      playerWin = true;
+    else if (computerPoints >= 3)
+    computerWin = true;
+  }
+
+  // check diag
+  if ((gameState[0] === "X" && gameState[4] === "X" && gameState[8] === "X")
+    || (gameState[2] === "X" && gameState[4] === "X" && gameState[6] === "X"))
+    playerWin = true;
+  else if ((gameState[0] === "X" && gameState[4] === "X" && gameState[8] === "X")
+    || (gameState[2] === "X" && gameState[4] === "X" && gameState[6] === "X"))
+    computerWin = true;
+
+  return playerWin ? 2 : (computerWin ? 1 : 0);
+}
