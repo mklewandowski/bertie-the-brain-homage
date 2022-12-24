2571f553964fa34d46ff6871222a15d6beb18436
mklewandowski <matt.kain.lewandowski@gmail.com>
2022-12-23 19:51:27 -0800

    Refactor and use calculateLinePoints to compute how many player and computer points are in each line

--- src/utils.ts
+++ src/utils.ts
@@ -5,6 +5,11 @@ export enum GameResultType {
   tie = "tie"
 }
 
+interface LinePoints {
+  computerPoints: number,
+  playerPoints: number,
+}
+
 const kGridSize = 3;
 
 const getRandomSquare = (gameState: string[]) => {
@@ -18,92 +23,87 @@ const getRandomSquare = (gameState: string[]) => {
   return bertieSquare;
 }
 
-const calculateWinPoints = (cellState: string, cellWinPoints: number, computerPoints: number, playerPoints: number) => {
+const calculateWinPoints = (cellState: string, cellWinPoints: number, linePoints: LinePoints) => {
   let newCellWinPoints = cellWinPoints;
   if (cellState !== "")
     newCellWinPoints = -1;
-  else if (computerPoints === 2) // can win
+  else if (linePoints.computerPoints === 2) // can win
     newCellWinPoints = 6;
-  else if (playerPoints === 2) // must block
+  else if (linePoints.playerPoints === 2) // must block
     newCellWinPoints = 5;
-  else if (playerPoints === 0) // can still win this row
+  else if (linePoints.playerPoints === 0) // can still win this row
     newCellWinPoints = Math.max(newCellWinPoints, newCellWinPoints + 1);
   return newCellWinPoints;
 }
 
+const calculateLinePoints = (cellState: string, linePoints: LinePoints) => {
+  let newLinePoints: LinePoints = { ...linePoints };
+  if (cellState === "X")
+    newLinePoints.playerPoints++;
+  else if (cellState === "O")
+    newLinePoints.computerPoints++;
+  return newLinePoints;
+}
+
 export const getBertieMove = (gameState: string[], difficulty: number) => {
   let bertieSquare = -1;
+  let linePoints: LinePoints = { computerPoints: 0, playerPoints: 0 };
   if (difficulty === 0) {
     bertieSquare = getRandomSquare(gameState);
   } else {
     const winPoints = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-    let computerPoints = 0;
-    let playerPoints = 0;
     // check rows
     for (let r = 0; r < kGridSize; r++) {
-      computerPoints = 0;
-      playerPoints = 0;
+      linePoints = { computerPoints: 0, playerPoints: 0 };
       for (let cell = 0; cell < kGridSize; cell++)
       {
-        if (gameState[r * kGridSize + cell] === "X")
-          playerPoints++;
-        else if (gameState[r * kGridSize + cell] === "O")
-          computerPoints++;
+        linePoints = calculateLinePoints(gameState[r * kGridSize + cell], linePoints);
       }
       for (let cell = 0; cell < kGridSize; cell++)
       {
-        winPoints[r * kGridSize + cell] = calculateWinPoints(gameState[r * kGridSize + cell], winPoints[r * kGridSize + cell], computerPoints, playerPoints);
+        winPoints[r * kGridSize + cell] = calculateWinPoints(gameState[r * kGridSize + cell], winPoints[r * kGridSize + cell], linePoints);
       }
     }
     // check cols
     for (let c = 0; c < kGridSize; c++) {
-      computerPoints = 0;
-      playerPoints = 0;
+      linePoints = { computerPoints: 0, playerPoints: 0 };
       for (let cell = 0; cell < kGridSize; cell++)
       {
-        if (gameState[c + kGridSize * cell] === "X")
-          playerPoints++;
-        else if (gameState[c + kGridSize * cell] === "O")
-          computerPoints++;
+        linePoints = calculateLinePoints(gameState[c + kGridSize * cell], linePoints);
       }
       for (let cell = 0; cell < kGridSize; cell++)
       {
-        winPoints[c + kGridSize * cell] = calculateWinPoints(gameState[c + kGridSize * cell], winPoints[c + kGridSize * cell], computerPoints, playerPoints);
+        winPoints[c + kGridSize * cell] = calculateWinPoints(gameState[c + kGridSize * cell], winPoints[c + kGridSize * cell], linePoints);
       }
     }
     // check diags
-    computerPoints = 0;
-    playerPoints = 0;
+    linePoints = { computerPoints: 0, playerPoints: 0 };
     for (let c = 0; c <= 8; c+=4)
     {
-      if (gameState[c] === "X")
-        playerPoints++;
-      else if (gameState[c] === "O")
-        computerPoints++;
+      linePoints = calculateLinePoints(gameState[c], linePoints);
+      console.log(linePoints);
     }
-    for (let cell = 0; cell < 8; cell+=4)
+    for (let cell = 0; cell <= 8; cell+=4)
     {
-      winPoints[cell] = calculateWinPoints(gameState[cell], winPoints[cell], computerPoints, playerPoints);
+      winPoints[cell] = calculateWinPoints(gameState[cell], winPoints[cell], linePoints);
     }
 
-    computerPoints = 0;
-    playerPoints = 0;
+    linePoints = { computerPoints: 0, playerPoints: 0 };
     for (let c = 2; c <= 6; c+=2)
     {
-      if (gameState[c] === "X")
-        playerPoints++;
-      else if (gameState[c] === "O")
-        computerPoints++;
+      console.log(linePoints);
+      linePoints = calculateLinePoints(gameState[c], linePoints);
     }
     for (let cell = 2; cell <= 6; cell+=2)
     {
-      winPoints[cell] = calculateWinPoints(gameState[cell], winPoints[cell], computerPoints, playerPoints);
+      winPoints[cell] = calculateWinPoints(gameState[cell], winPoints[cell], linePoints);
     }
 
     // find best cell
     let highVal = -1;
     for (let c = 0; c < winPoints.length; c++)
     {
+      console.log(c + ":" + winPoints[c]);
       if (winPoints[c] > highVal && (difficulty == 2 || highVal < 0 || (difficulty == 1 && Math.random() < .5))) {
         highVal = winPoints[c];
         bertieSquare = c;
